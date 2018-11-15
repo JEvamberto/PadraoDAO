56779afe3905ead74cbc8070511c34e864d79405
José Evamberto <evamberto.lima@gmail.com>
2018-11-15 20:48:32 -0300

    Primeiro Commit

---
+++ DaoAgenda/src/connection/ConnectionFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+/**
+ *
+ * @author jose
+ */
+public class ConnectionFactory {
+    
+   private static String DRIVER="";
+   private static String URL="";
+   private static String USER="";
+   private static String PASSWORD="";
+   
+   
+    
+    
+}
---
+++ DaoAgenda/src/model/Agenda.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author jose
+ */
+public class Agenda {
+    
+    private Email email;
+    private Nome nome;
+    private Telefone telefone;
+    
+}
